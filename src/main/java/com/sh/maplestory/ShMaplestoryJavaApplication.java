20448679e2e2d78b9c4184fba80b4f43e4781439
keencho <seyoung050412@gmail.com>
2024-02-18 20:49:45 +0900

    git init

---
+++ src/main/java/com/sh/maplestory/ShMaplestoryJavaApplication.java
@@ -0,0 +1,13 @@
+package com.sh.maplestory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShMaplestoryJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShMaplestoryJavaApplication.class, args);
+    }
+
+}
